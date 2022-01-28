6cbd978728030d6af4c36b23c5dc1ec7ff1dee3f
klkjjj4848 <klkjjj4848@naver.com>
2022-01-28 10:36:11 +0900

    3. 예외처리 공부 완료

--- src/site/metacoding/ex21/DivideByZero2.java
+++ src/site/metacoding/ex21/DivideByZero2.java
@@ -3,18 +3,13 @@ package site.metacoding.ex21;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class DivideByZero {
+public class DivideByZero2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         // InputMismatchException
 
         // 통신, 사용자 입력 예외처리!!
-
-        // 버퍼를 비우던, 스캐너를 새로 만들던 해야 함.
-
-        while (true) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("반복 시작");
-        }
 
         try {
             int x = sc.nextInt();
@@ -22,10 +17,9 @@ public class DivideByZero {
             System.out.println("받은 값 : " + x);
         } catch (Exception e) {
             System.out.println("============================================================");
-            System.out.println("문자를 넣지마세요.");
+            System.out.println(e.getMessage());
             System.out.println("============================================================");
             e.printStackTrace();
-            sc.nextLine(); // 버퍼 비우기!!
         }
     }
 }
